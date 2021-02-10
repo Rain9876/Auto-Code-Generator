2e42d6ff095964ab03ae6c2b329462c1886a2bf7
yurunsong <songyr888@gmail.com>
2021-02-10 14:55:04 +0800

    Add CSN Dataset for downstream

--- dataset/Dataset.py
+++ dataset/Dataset.py
@@ -5,88 +5,17 @@ from pathlib import Path
 from Utils import pretrained_data_preprocess
 import gc
 
-T5_PREFIX = "summarize: "
 
+
+T5_PREFIX = "translate NL to Python: "
 
 ########################################################################################################################
 # Finetuning Datasets
 ########################################################################################################################
 
-def encode_mimic_file(
-        tokenizer,
-        data_path,
-        max_src_length,
-        max_tgt_length,
-        numOfSamples,
-        overwrite_cache=False,
-        data_type="",
-        prefix=T5_PREFIX):
-    cache_path_src = Path(f"{data_path}_{data_type}_src.pt")
-    cache_path_tgt = Path(f"{data_path}_{data_type}_tgt.pt")
-
-    if not overwrite_cache and cache_path_src.exists() and cache_path_tgt.exists():
-        try:
-            examples_src = torch.load(cache_path_src)
-            assert isinstance(examples_src, list)
-            examples_tgt = torch.load(cache_path_tgt)
-            assert isinstance(examples_tgt, list)
-            print(f"Load {data_type} dataset successful.")
-            if numOfSamples:
-                examples_src = examples_src[:numOfSamples]
-                examples_tgt = examples_tgt[:numOfSamples]
-            return examples_src, examples_tgt
-
-        except Exception:
-            print(f"failed to load from {cache_path_src}, retokenizing {data_path}")
-
-    data_path = Path(f"{data_path}/{data_type}.csv")
-
-    data = pd.read_csv(data_path, names=["Source", "Target"], header=None)
-
-    if numOfSamples < len(data):
-        data = data.sample(n=numOfSamples, replace=False)
-        print(len(data))
-
-    data.Source = data.Source + "</s>"
-    data.Target = data.Target + "</s>"
-
-    if not prefix:
-        data.Source = prefix + data.Source
-
-    tokenized0 = tokenizer.batch_encode_plus(
-        data["Source"].values.tolist(),
-        max_length=max_src_length,
-        pad_to_max_length=True,
-        add_prefix_space=True,
-        truncation=True,
-        return_tensors="pt"
-    )
-
-    assert tokenized0.input_ids.shape[1] == max_src_length
-
-    tokenized1 = tokenizer.batch_encode_plus(
-        data["Target"].values.tolist(),
-        max_length=max_tgt_length,
-        pad_to_max_length=True,
-        add_prefix_space=True,
-        truncation=True,
-        return_tensors="pt"
-    )
-
-    assert tokenized1.input_ids.shape[1] == max_tgt_length
-
-    examples_src = [{"input_ids": tokenized0["input_ids"][i],
-                     "attention_mask": tokenized0["attention_mask"][i]} for i in range(len(data))]
-    examples_tgt = [{"input_ids": tokenized1["input_ids"][i],
-                     "attention_mask": tokenized1["attention_mask"][i]} for i in range(len(data))]
-
-    torch.save(examples_src, cache_path_src.open("wb"))
-    torch.save(examples_tgt, cache_path_tgt.open("wb"))
-
-    return examples_src, examples_tgt
-
 
 # Removing the padding token for generation
+
 def trim_batch(
         input_ids, pad_token_id, attention_mask=None,
 ):
@@ -96,11 +25,96 @@ def trim_batch(
     else:
         return (input_ids[:, keep_column_mask], attention_mask[:, keep_column_mask])
 
+def encode_CSN_file(
+        tokenizer,
+        data_path,
+        max_src_length,
+        max_tgt_length,
+        numOfSamples,
+        overwrite_cache=False,
+        data_type="",
+        prefix=T5_PREFIX):
 
-class MIMICDataset(Dataset):
-    def __init__(self, data_path, tokenizer, max_src_length=512, max_tgt_length=128, data_type="train",
-                 numOfSamples=10000):
-        self.data_path = data_path
+        cache_path_src = Path(f"{data_path}_{data_type}_src.pt")
+        cache_path_tgt = Path(f"{data_path}_{data_type}_tgt.pt")
+
+        if not overwrite_cache and cache_path_tgt.exists() and cache_path_src.exists():
+            try:
+                cache_src = torch.load(cache_path_src)
+                cache_tgt = torch.load(cache_path_tgt)
+
+                assert isinstance(cache_src, list)
+                assert isinstance(cache_tgt, list)
+
+                print(f"Load {data_type} dataset successful.")
+
+                if numOfSamples:
+                    cache_src = cache_src[:numOfSamples]
+                    cache_tgt = cache_tgt[:numOfSamples]
+
+                return cache_src, cache_tgt
+
+            except Exception:
+
+                print(f"failed to load from cache, retokenizing at {data_path}")
+
+
+        data_path = Path(f"{data_path}/{data_type}.csv")
+
+        data = pd.read_csv(data_path, names=["docstring", "code"], header=None)
+
+        if numOfSamples < len(data):
+            data = data.sample(n=numOfSamples, replace=False)
+        
+        print(f"Load {len(data)} {data_type} data.")
+
+
+        data.docstring = data.docstring + "</s>"
+        data.code = data.code + "</s>"
+
+        if not prefix:
+            data.docstring = prefix + data.docstring
+
+        tokenized_src = tokenizer.batch_encode_plus(
+            data["docstring"].values.tolist(),
+            max_length=max_src_length,
+            pad_to_max_length=True,
+            add_prefix_space=True,
+            truncation=True,
+            return_tensors="pt"
+        )
+
+        assert tokenized_src.input_ids.shape[1] == max_src_length
+
+        tokenized_tgt = tokenizer.batch_encode_plus(
+            data["code"].values.tolist(),
+            max_length=max_tgt_length,
+            pad_to_max_length=True,
+            add_prefix_space=True,
+            truncation=True,
+            return_tensors="pt"
+        )
+
+        assert tokenized_tgt.input_ids.shape[1] == max_tgt_length
+
+        encoded_src = [{"input_ids": tokenized_src["input_ids"][i],
+                         "attention_mask": tokenized_src["attention_mask"][i]} for i in range(len(data))]
+
+        encoded_tgt = [{"input_ids": tokenized_tgt["input_ids"][i],
+                         "attention_mask": tokenized_tgt["attention_mask"][i]} for i in range(len(data))]
+
+        torch.save(encoded_src, cache_path_src.open("wb"))
+
+        torch.save(encoded_tgt, cache_path_tgt.open("wb"))
+
+        return encoded_src, encoded_tgt
+
+
+
+
+class CSN_Dataset(Dataset):
+    def __init__(self, data_path, tokenizer, max_src_length=512, max_tgt_length=512, data_type="train", numOfSamples=100000)
+        
         self.tok = tokenizer
         self.max_src_length = max_src_length
         self.max_tgt_len = max_tgt_length
@@ -125,11 +139,15 @@ class MIMICDataset(Dataset):
         return source_ids, source_mask, y
 
 
+
+
+
 ########################################################################################################################
 # Pretraining Datasets
 ########################################################################################################################
 
 def encode_pretrain_file(
+
         tokenizer,
         data_path,
         max_src_length,
@@ -138,6 +156,7 @@ def encode_pretrain_file(
         overwrite_cache=False,
         data_type="",
         prefix=None):
+
     cache_path_src = Path(f"{data_path}/data_{data_type}_src.pt")
     cache_path_tgt = Path(f"{data_path}/data_{data_type}_tgt.pt")
 
@@ -156,7 +175,7 @@ def encode_pretrain_file(
         except Exception:
             print(f"failed to load from {cache_path_src}, retokenizing {data_path}")
 
-    print(f"Processing pretrain_{data_type}")
+    print(f"Processing Pretrained_{data_type}")
 
     encode_input, decode_input = pretrained_data_preprocess(tokenizer, f"{data_path}/pretrain_{data_type}.csv",
                                                             numOfSamples)
