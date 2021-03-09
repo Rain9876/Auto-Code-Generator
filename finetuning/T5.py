34bfef4ee53fe2b56c86675b35266742fe676470
junlin <junlinwang18@gmail.com>
2021-03-08 20:13:58 -0800

    workable RL code

--- finetuning/T5.py
+++ finetuning/T5.py
@@ -1,29 +1,107 @@
+import os
+import sys
+
+from numpy.core.fromnumeric import size
+
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+
+import torch
 from torch.utils.data import DataLoader
 from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
 from Utils import *
 from dataset.Dataset import CSN_Dataset
 import collections
 from apex import amp
+from rouge_score import rouge_scorer, scoring
+import numpy as np
 
 set_rand_seeds()
 device = set_device()
+scorer = rouge_scorer.RougeScorer(["rouge1", "rougeL"], use_stemmer=True)
 
 
-def training_per_iteration(model, tokenizer, data, optimizer, lr_sch, amp):
+def reward_control_flow(x):
+    return 0
+
+
+def reward_bleu(x):
+    return x
+
+
+def reward_rogue(x, y):
+    scores = scorer.score(x, y)
+    return scores
+
+
+def Policy_Gradient_Update(model, tokenizer, data):
+    batch_size = data["input_ids"].shape[0]
+    T = data["input_ids"].shape[1]
+    y = data["decoder_input_ids"].to(device, dtype=torch.long)
+    lm_labels = y.clone().detach()
+    lm_labels[y == tokenizer.pad_token_id] = -100
+    ids = data["input_ids"].to(device, dtype=torch.long)
+    mask = data["attention_mask"].to(device, dtype=torch.long)
+    ## 1. compute rewards
+    # # outputs = model(input_ids=ids, attention_mask=mask)
+    # # (B,S,V)
+    # # logits = outputs.logits
+    # # output_ids = torch.
+
+    print()
+    outputs = model.generate(ids, max_length=50,)
+    # print("generated outputs:", outputs)
+    out_string = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    y_string = tokenizer.decode(y[0], skip_special_tokens=True)
+    print("string:", out_string)
+    # print("y:", y)
+    RC = reward_control_flow(outputs)
+    RS = reward_rogue(out_string, y_string)["rougeL"].fmeasure
+    print("Rogue Score: {}".format(RS))
+    averageRC = np.mean(RC)
+    averageRS = np.mean(RS)
+    unbiasedRC = RC  # - averageRC
+    unbiasedRS = RS  # - averageRS
+    # might have to split these 2 rewards into 2 policy updates.
+    total_rewards = torch.tensor(unbiasedRC + unbiasedRS, dtype=torch.float).to(device)
+    ## 2 compute log p(x_t|x_<t), the label doesn't actually matter ?
+    outputs = model(input_ids=ids, attention_mask=mask, labels=lm_labels)
+    # (B,S,V)
+    lm_logits = outputs.logits
+    # print(lm_logits.shape)
+    loss_fct = torch.nn.CrossEntropyLoss(ignore_index=-100, reduction="none")
+    loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), lm_labels.view(-1))
+    # print(f"loss:{loss.shape}")
+    # print(total_rewards)
+    # (B,1)
+    summed_loss = torch.sum(loss, dim=0) * 1 / T
+    weighted_loss = torch.dot(total_rewards.unsqueeze(0), summed_loss.unsqueeze(0))
+
+    ## 3 Loss
+    L = 1 / batch_size * (weighted_loss)
+
+    return L
+
+
+def training_per_iteration(model, tokenizer, data, optimizer, lr_sch, pg, amp):
     optimizer.zero_grad()
 
-    y = data['decoder_input_ids'].to(device, dtype=torch.long)
+    y = data["decoder_input_ids"].to(device, dtype=torch.long)
 
     lm_labels = y.clone().detach()
     lm_labels[y == tokenizer.pad_token_id] = -100
 
-    ids = data['input_ids'].to(device, dtype=torch.long)
-    mask = data['attention_mask'].to(device, dtype=torch.long)
+    ids = data["input_ids"].to(device, dtype=torch.long)
+    mask = data["attention_mask"].to(device, dtype=torch.long)
 
     outputs = model(input_ids=ids, attention_mask=mask, labels=lm_labels)
 
-    loss = outputs[0]
+    if pg:
+        pg_loss = Policy_Gradient_Update(model, tokenizer, data)
+        print(f"NLL{ outputs[0]}, final pg loss:{pg_loss}")
 
+        loss = outputs[0] + pg_loss
+    else:
+        loss = outputs[0]
     with amp.scale_loss(loss, optimizer) as scaled_loss:
         scaled_loss.backward()
 
@@ -36,7 +114,7 @@ def training_per_iteration(model, tokenizer, data, optimizer, lr_sch, amp):
     return loss
 
 
-def validate(tokenizer, model, loader, generate=True, interval = 1000):
+def validate(tokenizer, model, loader, generate=True, interval=1000):
 
     model.eval()
     val_loss = []
@@ -47,7 +125,9 @@ def validate(tokenizer, model, loader, generate=True, interval = 1000):
 
         for iteration, data in enumerate(loader, 1):
 
-            source_ids, source_mask, y = CSN_Dataset.trim_seq2seq_batch(data, tokenizer.pad_token_id)
+            source_ids, source_mask, y = CSN_Dataset.trim_seq2seq_batch(
+                data, tokenizer.pad_token_id
+            )
 
             y = y.to(device, dtype=torch.long)
 
@@ -69,17 +149,27 @@ def validate(tokenizer, model, loader, generate=True, interval = 1000):
                     attention_mask=mask,
                     max_length=300,
                     num_beams=4,
-                    early_stopping=True
+                    early_stopping=True,
                 )
 
-                preds = [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=True) for g in
-                         generated_ids]
-                target = [tokenizer.decode(t, skip_special_tokens=True, clean_up_tokenization_spaces=True) for t in y]
+                preds = [
+                    tokenizer.decode(
+                        g, skip_special_tokens=True, clean_up_tokenization_spaces=True
+                    )
+                    for g in generated_ids
+                ]
+                target = [
+                    tokenizer.decode(
+                        t, skip_special_tokens=True, clean_up_tokenization_spaces=True
+                    )
+                    for t in y
+                ]
                 gen_time = (time.time() - t0) / ids.shape[0]
 
                 if iteration % 100 == 0:
                     print(
-                        f'[Generation] batch: {iteration}/{len(loader)}, Loss: {sum(val_loss[-100:]) / 100}, Time per gen: {gen_time}s')
+                        f"[Generation] batch: {iteration}/{len(loader)}, Loss: {sum(val_loss[-100:]) / 100}, Time per gen: {gen_time}s"
+                    )
 
                 predictions.extend(preds)
                 actuals.extend(target)
@@ -87,56 +177,78 @@ def validate(tokenizer, model, loader, generate=True, interval = 1000):
             else:
                 if iteration % interval == 0:
                     print(
-                        f'[Validation] batch: {iteration}/{len(loader)}, Loss: {sum(val_loss[-interval:]) / interval}')
+                        f"[Validation] batch: {iteration}/{len(loader)}, Loss: {sum(val_loss[-interval:]) / interval}"
+                    )
 
     return predictions, actuals, val_loss
 
 
-
-def save_checkpoint(step, model, opt, lr_sch, loss, min_loss, amp, best_model=True, last_model=True, suffix=""):
-    checkpoint = {'step': step, 'model_state': model.state_dict(), "optimizer_state": opt.state_dict(),
-                  "lr_sch_state": lr_sch.state_dict(), "amp": amp.state_dict()}
+def save_checkpoint(
+    step,
+    model,
+    opt,
+    lr_sch,
+    loss,
+    min_loss,
+    amp,
+    best_model=True,
+    last_model=True,
+    suffix="",
+):
+    checkpoint = {
+        "step": step,
+        "model_state": model.state_dict(),
+        "optimizer_state": opt.state_dict(),
+        "lr_sch_state": lr_sch.state_dict(),
+        "amp": amp.state_dict(),
+    }
+    folder = "pg/"
     if last_model:
-        model_name = f'last_model{suffix}.ckpt'
+        model_name = folder + f"last_model{suffix}.ckpt"
         torch.save(checkpoint, model_name)
-        print(f'[Info] The last model on step {step} has been saved.')
+        print(f"[Info] The last model on step {step} has been saved.")
 
     if best_model:
         if loss <= min_loss:
-            model_name = f'best_model_loss{suffix}.ckpt'
+            model_name = folder + f"best_model_loss{suffix}.ckpt"
             torch.save(checkpoint, model_name)
-            print('[Info] The best model loss checkpoint file has been updated.')
-
+            print("[Info] The best model loss checkpoint file has been updated.")
 
 
 def load_checkpoint(PATH, model, opt, lr_sch, opt_level):
     checkpoint = torch.load(PATH)
 
     model, opt = amp.initialize(model, opt, opt_level=opt_level)
-    model.load_state_dict(checkpoint['model_state'])
-    opt.load_state_dict(checkpoint['optimizer_state'])
-    step = checkpoint['step']+1
-    lr_sch = lr_sch.load_state_dict(checkpoint['lr_sch_state'])
+    model.load_state_dict(checkpoint["model_state"])
+    opt.load_state_dict(checkpoint["optimizer_state"])
+    step = checkpoint["step"] + 1
+    lr_sch = lr_sch.load_state_dict(checkpoint["lr_sch_state"])
 
     return model, opt, step, lr_sch
 
 
-
 def data_process(config, tokenizer, data_type, data_size):
     # Creating the Training and Validation dataset for further creation of Dataloader
-    data_set = CSN_Dataset(config.DATA_DIR, tokenizer, config.MAX_SRC_LEN, config.MAX_TGT_LEN, data_type, data_size)
+    data_set = CSN_Dataset(
+        config.DATA_DIR,
+        tokenizer,
+        config.MAX_SRC_LEN,
+        config.MAX_TGT_LEN,
+        data_type,
+        data_size,
+    )
 
     # Defining the parameters for creation of dataloaders
     train_params = {
-        'batch_size': config.TRAIN_BATCH_SIZE,
-        'shuffle': False,
-        'num_workers': 4
+        "batch_size": config.TRAIN_BATCH_SIZE,
+        "shuffle": False,
+        "num_workers": 4,
     }
 
     val_params = {
-        'batch_size': config.VALID_BATCH_SIZE,
-        'shuffle': False,
-        'num_workers': 2
+        "batch_size": config.VALID_BATCH_SIZE,
+        "shuffle": False,
+        "num_workers": 2,
     }
 
     # Creation of Dataloaders for testing and validation. This will be used down for training and validation stage for the model.
@@ -153,29 +265,42 @@ def data_process(config, tokenizer, data_type, data_size):
     return data_loader
 
 
-
-
 def fine_tuning():
 
-    param = collections.namedtuple('param', ["TRAIN_BATCH_SIZE", "VALID_BATCH_SIZE", "TEST_BATCH_SIZE", "TRAIN_EPOCHS", "TRAIN_STEPS",
-                                             "TEST_EPOCHS", "LEARNING_RATE", "MAX_SRC_LEN", "MAX_TGT_LEN", "DATA_DIR", "RESUME_PATH",
-                                             "RESUME"])
+    param = collections.namedtuple(
+        "param",
+        [
+            "TRAIN_BATCH_SIZE",
+            "VALID_BATCH_SIZE",
+            "TEST_BATCH_SIZE",
+            "TRAIN_EPOCHS",
+            "TRAIN_STEPS",
+            "TEST_EPOCHS",
+            "LEARNING_RATE",
+            "MAX_SRC_LEN",
+            "MAX_TGT_LEN",
+            "DATA_DIR",
+            "RESUME_PATH",
+            "RESUME",
+            "POLICY_GRADIENT",
+        ],
+    )
 
     config = param(
-        TRAIN_BATCH_SIZE= 8,  # input batch size for training_loader
-        VALID_BATCH_SIZE= 4,  # input batch size for testing
-        TEST_BATCH_SIZE= 4,  # input batch size for testing
-        TRAIN_EPOCHS= 100,  # number of epochs to train
-        TRAIN_STEPS = 5000,
-        TEST_EPOCHS = 1,
+        TRAIN_BATCH_SIZE=1,  # input batch size for training_loader
+        VALID_BATCH_SIZE=4,  # input batch size for testing
+        TEST_BATCH_SIZE=4,  # input batch size for testing
+        TRAIN_EPOCHS=100,  # number of epochs to train
+        TRAIN_STEPS=5000,
+        TEST_EPOCHS=1,
         LEARNING_RATE=1e-5,  # learning rate (default: 0.01)
         MAX_SRC_LEN=512,
         MAX_TGT_LEN=512,
-        DATA_DIR = "/home/song/Desktop/Auto-Code-Generator/data_processing/processed_data/finetuning",
-        RESUME_PATH = "",
-        RESUME=False
+        DATA_DIR="/home/junliw/CodeGeneration/Auto-Code-Generator/data_processing/processed_data/finetuning",
+        RESUME_PATH="",
+        RESUME=False,
+        POLICY_GRADIENT=True,
     )
-
 
     # tokenzier for encoding the text
     tokenizer = T5Tokenizer.from_pretrained("t5-base")
@@ -187,11 +312,11 @@ def fine_tuning():
 
     # Defining the optimizer that will be used to tune the weights of the network in the training session.
     optimizer = torch.optim.AdamW(params=model.parameters(), lr=config.LEARNING_RATE)
-    
+
     # Defining the LR scheduler with warm up
     lr_sch = LinearWarmupRsqrtDecayLR(optimizer, 1000)
 
-    print('Initiating Finetuning for the T5 model on dataset')
+    print("Initiating Finetuning for the T5 model on dataset")
 
     step = 0
     epoch = 0
@@ -207,13 +332,15 @@ def fine_tuning():
     val_interval = 100
 
     opt_level = "O1"
-    model, optimizer = amp.initialize(model,optimizer, opt_level=opt_level)
+    model, optimizer = amp.initialize(model, optimizer, opt_level=opt_level)
 
     # Load Saved Model
     if config.RESUME:
         print("Load saved model!")
         print("Path: ", config.RESUME_PATH)
-        model, optimizer, step, lr_sch = load_checkpoint(config.RESUME_PATH, model, optimizer, lr_sch, opt_level)
+        model, optimizer, step, lr_sch = load_checkpoint(
+            config.RESUME_PATH, model, optimizer, lr_sch, opt_level
+        )
         print("step: ", step)
 
     training_loader = data_process(config, tokenizer, "train", 5000)
@@ -230,7 +357,9 @@ def fine_tuning():
 
             model.train()
 
-            loss = training_per_iteration(model, tokenizer, data, optimizer, lr_sch, amp)
+            loss = training_per_iteration(
+                model, tokenizer, data, optimizer, lr_sch, config.POLICY_GRADIENT, amp
+            )
 
             avg_loss.append(loss.item())
 
@@ -238,40 +367,42 @@ def fine_tuning():
 
             if step % train_interval == 0:
                 print(
-                    f'Steps: {step}, batch: {step}/{config.TRAIN_STEPS}, Loss:  {sum(avg_loss[-train_interval:]) / train_interval}, lr: {lr_sch.get_lr()[0]}')
+                    f"Steps: {step}, batch: {step}/{config.TRAIN_STEPS}, Loss:  {sum(avg_loss[-train_interval:]) / train_interval}, lr: {lr_sch.get_lr()[0]}"
+                )
                 train_loss_ = sum(avg_loss) / len(avg_loss)
                 train_loss.append(train_loss_)
-
 
             if step % val_interval == 0:
                 _, _, val_loss_ = validate(tokenizer, model, val_loader, generate=False)
                 val_avg_loss = sum(val_loss_) / len(val_loss_)
                 print("Average val loss ", val_avg_loss)
                 val_loss.append(val_avg_loss)
-                save_checkpoint(step, model, optimizer, lr_sch, val_avg_loss, min(val_loss), amp)  # Early stop for best Loss
+                save_checkpoint(
+                    step, model, optimizer, lr_sch, val_avg_loss, min(val_loss), amp
+                )  # Early stop for best Loss
 
-
-     # Testing
-    prediction, actual, test_loss = validate(tokenizer, model, test_loader, generate=True)
+    # Testing
+    prediction, actual, test_loss = validate(
+        tokenizer, model, test_loader, generate=True
+    )
     rouge = calculate_rouge(prediction, actual)
     metrics["rouge"].append(rouge)
     print("Rouge score", rouge)
-    final_df = pd.DataFrame({'Generated Code': prediction, 'Actual Code': actual})
-    final_df.to_csv('./predictions.csv')
-    print('Output Files generated for review')
-    
+    final_df = pd.DataFrame({"Generated Code": prediction, "Actual Code": actual})
+    final_df.to_csv("./predictions.csv")
+    print("Output Files generated for review")
 
     Info = {
-        "train_loss": train_loss, 
+        "train_loss": train_loss,
         "val_loss": val_loss,
-        "test_loss" : test_loss,  
-        "rouge": rouge, 
+        "test_loss": test_loss,
+        "rouge": rouge,
         "num_of_epochs": config.TRAIN_EPOCHS,
-        "num_of_steps": step
+        "num_of_steps": step,
     }
 
     saveInfo(Info)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     fine_tuning()
