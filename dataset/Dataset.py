f5bda3fb6b77550a944fac8b1be6c9d62b8552cf
Rain9876 <songyr888@gmail.com>
2021-03-16 18:13:19 +0800

    Add RL into T5 finetuning Model

--- dataset/Dataset.py
+++ dataset/Dataset.py
@@ -7,6 +7,7 @@ import gc
 
 T5_PREFIX = "translate NL to Python: "
 
+
 ########################################################################################################################
 # Finetuning Datasets
 ########################################################################################################################
@@ -23,98 +24,107 @@ def trim_batch(
     else:
         return (input_ids[:, keep_column_mask], attention_mask[:, keep_column_mask])
 
+
 def encode_CSN_file(
         tokenizer,
         data_path,
         max_src_length,
         max_tgt_length,
-        numOfSamples,
+        start_idx,
+        end_idx,
         overwrite_cache=False,
         data_type="",
         prefix=T5_PREFIX):
 
-        cache_path_src = Path(f"{data_path}_{data_type}_src.pt")
-        cache_path_tgt = Path(f"{data_path}_{data_type}_tgt.pt")
+    cache_path_src = Path(f"{data_path}_{data_type}_src.pt")
+    cache_path_tgt = Path(f"{data_path}_{data_type}_tgt.pt")
 
-        if not overwrite_cache and cache_path_tgt.exists() and cache_path_src.exists():
-            try:
-                cache_src = torch.load(cache_path_src)
-                cache_tgt = torch.load(cache_path_tgt)
+    if not overwrite_cache and cache_path_tgt.exists() and cache_path_src.exists():
 
-                assert isinstance(cache_src, list)
-                assert isinstance(cache_tgt, list)
+        try:
+            cache_src = torch.load(cache_path_src)
+            cache_tgt = torch.load(cache_path_tgt)
 
-                print(f"Load {data_type} dataset successful.")
+            assert isinstance(cache_src, list)
+            assert isinstance(cache_tgt, list)
 
-                if numOfSamples:
-                    cache_src = cache_src[:numOfSamples]
-                    cache_tgt = cache_tgt[:numOfSamples]
+            print(f"Load {data_type} cache successful.")
 
-                return cache_src, cache_tgt
+            if end_idx:
+                cache_src = cache_src[start_idx:end_idx]
+                cache_tgt = cache_tgt[start_idx:end_idx]
 
-            except Exception:
+            return cache_src, cache_tgt
 
-                print(f"failed to load from cache, retokenizing at {data_path}")
+        except Exception:
 
+            print(f"failed to load from cache, retokenizing at {data_path}")
 
-        data_path = Path(f"{data_path}/{data_type}.csv")
+    else:
+            # Process entire database
+            data_path = Path(f"{data_path}/{data_type}.csv")
 
-        data = pd.read_csv(data_path, names=["docstring", "code"], header=None)
+            data = pd.read_csv(data_path, names=["docstring", "code"], header=None)
 
-        if numOfSamples < len(data):
-            data = data.sample(n=numOfSamples, replace=False)
-        
-        print(f"Load {len(data)} {data_type} data.")
+            print(f"Load {len(data)} {data_type} data.")
 
-        if not prefix:
-            data.docstring = prefix + data.docstring
+            if not prefix:
+                data.docstring = prefix + data.docstring
 
-        tokenized_src = tokenizer.batch_encode_plus(
-            data["docstring"].values.tolist(),
-            max_length=max_src_length,
-            padding=True,
-            truncation=True,
-            return_tensors="pt"
-        )
+            # data = data.sample(frac=1)
 
-        assert tokenized_src.input_ids.shape[1] == max_src_length
+            tokenized_src = tokenizer.batch_encode_plus(
+                data["docstring"].values.tolist(),
+                max_length=max_src_length,
+                padding=True,
+                truncation=True,
+                return_tensors="pt"
+            )
 
-        tokenized_tgt = tokenizer.batch_encode_plus(
-            data["code"].values.tolist(),
-            max_length=max_tgt_length,
-            padding=True,
-            truncation=True,
-            return_tensors="pt"
-        )
+            assert tokenized_src.input_ids.shape[1] == max_src_length
 
-        assert tokenized_tgt.input_ids.shape[1] == max_tgt_length
+            tokenized_tgt = tokenizer.batch_encode_plus(
+                data["code"].values.tolist(),
+                max_length=max_tgt_length,
+                padding=True,
+                truncation=True,
+                return_tensors="pt"
+            )
 
-        encoded_src = [{"input_ids": tokenized_src["input_ids"][i],
-                         "attention_mask": tokenized_src["attention_mask"][i]} for i in range(len(data))]
+            assert tokenized_tgt.input_ids.shape[1] == max_tgt_length
 
-        encoded_tgt = [{"input_ids": tokenized_tgt["input_ids"][i],
-                         "attention_mask": tokenized_tgt["attention_mask"][i]} for i in range(len(data))]
+            encoded_src = [{"input_ids": tokenized_src["input_ids"][i],
+                            "attention_mask": tokenized_src["attention_mask"][i]} for i in range(len(data))]
 
-        torch.save(encoded_src, cache_path_src.open("wb"))
+            encoded_tgt = [{"input_ids": tokenized_tgt["input_ids"][i],
+                            "attention_mask": tokenized_tgt["attention_mask"][i]} for i in range(len(data))]
 
-        torch.save(encoded_tgt, cache_path_tgt.open("wb"))
+            torch.save(encoded_src, cache_path_src.open("wb"))
 
-        return encoded_src, encoded_tgt
+            torch.save(encoded_tgt, cache_path_tgt.open("wb"))
 
+            if end_idx > len(data):
+                end_idx = len(data)
 
+            return encoded_src[start_idx:end_idx], encoded_tgt[start_idx:end_idx]
 
 
 class CSN_Dataset(Dataset):
-    def __init__(self, data_path, tokenizer, max_src_length=512, max_tgt_length=512, data_type="train", numOfSamples=100000):
-        self.data_path = data_path
-        self.tok = tokenizer
-        self.max_src_length = max_src_length
-        self.max_tgt_len = max_tgt_length
-        self.numOfSamples = numOfSamples
+    '''
+        CSN: CodeSearchNet Dataset
 
-        self.source, self.target = encode_CSN_file(self.tok, self.data_path, self.max_src_length, self.max_tgt_len,
-                                                     self.numOfSamples, overwrite_cache=True, data_type=data_type,
-                                                     prefix=T5_PREFIX)
+    '''
+
+    def __init__(self, data_path, tokenizer, max_src_length=512, max_tgt_length=512, data_type="train", start_idx=0,
+                 end_idx=0):
+        self.source, self.target = encode_CSN_file(tokenizer,
+                                                   data_path,
+                                                   max_src_length,
+                                                   max_tgt_length,
+                                                   start_idx,
+                                                   end_idx,
+                                                   overwrite_cache=False,  # Update Once Overwrite Needed
+                                                   data_type=data_type)
 
     def __len__(self):
         return len(self.source)
@@ -129,118 +139,3 @@ class CSN_Dataset(Dataset):
         y = trim_batch(batch["decoder_input_ids"], pad_token_id)
         source_ids, source_mask = trim_batch(batch["input_ids"], pad_token_id, attention_mask=batch["attention_mask"])
         return source_ids, source_mask, y
-
-
-
-
-
-########################################################################################################################
-# Pretraining Datasets
-########################################################################################################################
-#
-# def encode_pretrain_file(
-#
-#         tokenizer,
-#         data_path,
-#         max_src_length,
-#         max_tgt_length,
-#         numOfSamples=None,
-#         overwrite_cache=False,
-#         data_type="",
-#         prefix=None):
-#
-#     cache_path_src = Path(f"{data_path}/data_{data_type}_src.pt")
-#     cache_path_tgt = Path(f"{data_path}/data_{data_type}_tgt.pt")
-#
-#     if not overwrite_cache and cache_path_src.exists() and cache_path_tgt.exists():
-#         try:
-#             examples_src = torch.load(cache_path_src)
-#             assert isinstance(examples_src, list)
-#             examples_tgt = torch.load(cache_path_tgt)
-#             assert isinstance(examples_tgt, list)
-#             print(f"Load {data_type} dataset successful.")
-#             if numOfSamples:
-#                 examples_src = examples_src[:numOfSamples]
-#                 examples_tgt = examples_tgt[:numOfSamples]
-#             return examples_src, examples_tgt
-#
-#         except Exception:
-#             print(f"failed to load from {cache_path_src}, retokenizing {data_path}")
-#
-#     print(f"Processing Pretrained_{data_type}")
-#
-#     encode_input, decode_input = pretrained_data_preprocess(tokenizer, f"{data_path}/pretrain_{data_type}.csv",
-#                                                             numOfSamples)
-#
-#     print(f"Samples: {len(encode_input)}")
-#
-#     encode_input_tok = tokenizer.batch_encode_plus(
-#         encode_input,
-#         max_length=max_src_length,
-#         pad_to_max_length=True,
-#         add_prefix_space=True,
-#         truncation=True,
-#         return_tensors="pt"
-#     )
-#
-#     assert encode_input_tok.input_ids.shape[1] == max_src_length
-#
-#     del encode_input
-#     gc.collect()
-#     print(f"Input tokenization complete!")
-#
-#     decode_input_tok = tokenizer.batch_encode_plus(
-#         decode_input,
-#         max_length=max_tgt_length,
-#         pad_to_max_length=True,
-#         add_prefix_space=True,
-#         truncation=True,
-#         return_tensors="pt"
-#     )
-#
-#     assert decode_input_tok.input_ids.shape[1] == max_tgt_length
-#
-#     del decode_input
-#     gc.collect()
-#     print(f"Target tokenization complete!")
-#
-#     examples_src = [{"input_ids": encode_input_tok["input_ids"][i],
-#                      "attention_mask": encode_input_tok["attention_mask"][i]} for i in
-#                     range(len(encode_input_tok["input_ids"]))]
-#
-#     examples_tgt = [{"input_ids": decode_input_tok["input_ids"][i],
-#                      "attention_mask": decode_input_tok["attention_mask"][i]} for i in
-#                     range(len(decode_input_tok["input_ids"]))]
-#
-#     torch.save(examples_src, cache_path_src.open("wb"))
-#     torch.save(examples_tgt, cache_path_tgt.open("wb"))
-#
-#     return examples_src, examples_tgt
-#
-#
-# class PretrainDataset(Dataset):
-#
-#     def __init__(self, data_path, tokenizer, max_src_len=512, max_tgt_len=128, data_type="train", numOfSamples=None):
-#         self.data_path = data_path
-#         self.tok = tokenizer
-#         self.max_src_len = max_src_len
-#         self.max_tgt_len = max_tgt_len
-#         self.numOfSamples = numOfSamples
-#
-#         self.source, self.target = encode_pretrain_file(self.tok, self.data_path, self.max_src_len, self.max_tgt_len,
-#                                                         numOfSamples=self.numOfSamples,
-#                                                         overwrite_cache=False, data_type=data_type, prefix=None)
-#
-#     def __len__(self):
-#         return len(self.source)
-#
-#     def __getitem__(self, index):
-#         source_ids = self.source[index]["input_ids"].squeeze()
-#         target_ids = self.target[index]["input_ids"].squeeze()
-#         src_mask = self.source[index]["attention_mask"].squeeze()
-#         return {"input_ids": source_ids, "attention_mask": src_mask, "decoder_input_ids": target_ids}
-#
-#     def trim_seq2seq_batch(batch, pad_token_id):
-#         y = trim_batch(batch["decoder_input_ids"], pad_token_id)
-#         source_ids, source_mask = trim_batch(batch["input_ids"], pad_token_id, attention_mask=batch["attention_mask"])
-#         return source_ids, source_mask, y
--- finetuning/T5.py
+++ finetuning/T5.py
@@ -4,25 +4,36 @@ from Utils import *
 from dataset.Dataset import CSN_Dataset
 import collections
 from apex import amp
+from Evaluate import RL_of_loss
 
 set_rand_seeds()
 device = set_device()
 
 
 def training_per_iteration(model, tokenizer, data, optimizer, lr_sch, amp):
+    model.train()
+
     optimizer.zero_grad()
 
-    y = data['decoder_input_ids'].to(device, dtype=torch.long)
+    y = data['decoder_input_ids'].to(device)
 
     lm_labels = y.clone().detach()
     lm_labels[y == tokenizer.pad_token_id] = -100
 
-    ids = data['input_ids'].to(device, dtype=torch.long)
-    mask = data['attention_mask'].to(device, dtype=torch.long)
+    ids = data['input_ids'].to(device)
+    mask = data['attention_mask'].to(device)
 
     outputs = model(input_ids=ids, attention_mask=mask, labels=lm_labels)
 
     loss = outputs[0]
+
+    source_ids, source_mask, _ = CSN_Dataset.trim_seq2seq_batch(data, tokenizer.pad_token_id)
+    ids = source_ids.to(device)
+    mask = source_mask.to(device)
+
+    # print(ids)
+
+    loss = RL_of_loss(model, ids, y, mask, loss, tokenizer)
 
     with amp.scale_loss(loss, optimizer) as scaled_loss:
         scaled_loss.backward()
@@ -36,8 +47,7 @@ def training_per_iteration(model, tokenizer, data, optimizer, lr_sch, amp):
     return loss
 
 
-def validate(tokenizer, model, loader, generate=True, interval = 1000):
-
+def validate(tokenizer, model, loader, generate=True, interval=1000):
     model.eval()
     val_loss = []
     predictions = []
@@ -67,8 +77,7 @@ def validate(tokenizer, model, loader, generate=True, interval = 1000):
                 generated_ids = model.generate(
                     input_ids=ids,
                     attention_mask=mask,
-                    max_length=300,
-                    num_beams=4,
+                    num_beams=1,
                     early_stopping=True
                 )
 
@@ -92,7 +101,6 @@ def validate(tokenizer, model, loader, generate=True, interval = 1000):
     return predictions, actuals, val_loss
 
 
-
 def save_checkpoint(step, model, opt, lr_sch, loss, min_loss, amp, best_model=True, last_model=True, suffix=""):
     checkpoint = {'step': step, 'model_state': model.state_dict(), "optimizer_state": opt.state_dict(),
                   "lr_sch_state": lr_sch.state_dict(), "amp": amp.state_dict()}
@@ -108,23 +116,22 @@ def save_checkpoint(step, model, opt, lr_sch, loss, min_loss, amp, best_model=Tr
             print('[Info] The best model loss checkpoint file has been updated.')
 
 
-
-def load_checkpoint(PATH, model, opt, lr_sch, opt_level):
+def load_checkpoint(PATH, model, opt, lr_sch):
     checkpoint = torch.load(PATH)
 
-    model, opt = amp.initialize(model, opt, opt_level=opt_level)
+    # model, opt = amp.initialize(model, opt, opt_level=opt_level)
     model.load_state_dict(checkpoint['model_state'])
     opt.load_state_dict(checkpoint['optimizer_state'])
-    step = checkpoint['step']+1
+    step = checkpoint['step'] + 1
     lr_sch = lr_sch.load_state_dict(checkpoint['lr_sch_state'])
 
     return model, opt, step, lr_sch
 
 
-
-def data_process(config, tokenizer, data_type, data_size):
+def data_process(config, tokenizer, data_type, start_idx, end_idx):
     # Creating the Training and Validation dataset for further creation of Dataloader
-    data_set = CSN_Dataset(config.DATA_DIR, tokenizer, config.MAX_SRC_LEN, config.MAX_TGT_LEN, data_type, data_size)
+    data_set = CSN_Dataset(config.DATA_DIR, tokenizer, config.MAX_SRC_LEN, config.MAX_TGT_LEN, data_type, start_idx,
+                           end_idx)
 
     # Defining the parameters for creation of dataloaders
     train_params = {
@@ -143,9 +150,11 @@ def data_process(config, tokenizer, data_type, data_size):
     if "train" in data_type:
         data_loader = DataLoader(data_set, **train_params)
         print("Training_samples:", len(data_set))
+
     elif "val" in data_type:
         data_loader = DataLoader(data_set, **val_params)
         print("Val_samples:", len(data_set))
+
     elif "test" in data_type:
         data_loader = DataLoader(data_set, **val_params)
         print("Test_samples:", len(data_set))
@@ -153,32 +162,49 @@ def data_process(config, tokenizer, data_type, data_size):
     return data_loader
 
 
+def load_training_data_portion(config, tokenizer, current_portion, section=11):
+    # Assume 4K per portion and more than 40K for finetuning in total
+    # It needs 11 portion as a cycle.
+    # It's better to compute how many section in advance, given each volume of portion
+    # such as 4K * 11 =~ 40K
+
+
+    k = current_portion % section
+
+    # Load 4K training data, 412179 in total.
+    training_loader = data_process(config, tokenizer, "train", start_idx=40000 * k, end_idx=40000 * (k + 1))
+
+    print("Load Data Portion: ", current_portion % section)
+
+    return training_loader
 
 
 def fine_tuning():
-
-    param = collections.namedtuple('param', ["TRAIN_BATCH_SIZE", "VALID_BATCH_SIZE", "TEST_BATCH_SIZE", "TRAIN_EPOCHS", "TRAIN_STEPS",
-                                             "TEST_EPOCHS", "LEARNING_RATE", "MAX_SRC_LEN", "MAX_TGT_LEN", "DATA_DIR", "RESUME_PATH",
+    param = collections.namedtuple('param', ["TRAIN_BATCH_SIZE", "VALID_BATCH_SIZE", "TEST_BATCH_SIZE", "TRAIN_EPOCHS",
+                                             "TRAIN_STEPS",
+                                             "TEST_EPOCHS", "LEARNING_RATE", "MAX_SRC_LEN", "MAX_TGT_LEN", "DATA_DIR",
+                                             "RESUME_PATH",
                                              "RESUME"])
 
     config = param(
-        TRAIN_BATCH_SIZE= 8,  # input batch size for training_loader
-        VALID_BATCH_SIZE= 4,  # input batch size for testing
-        TEST_BATCH_SIZE= 4,  # input batch size for testing
-        TRAIN_EPOCHS= 100,  # number of epochs to train
-        TRAIN_STEPS = 5000,
-        TEST_EPOCHS = 1,
+        TRAIN_BATCH_SIZE=8,  # input batch size for training_loader
+        VALID_BATCH_SIZE=4,  # input batch size for testing
+        TEST_BATCH_SIZE=4,  # input batch size for testing
+        TRAIN_EPOCHS=100,  # number of epochs to train
+        TRAIN_STEPS=5000,
+        TEST_EPOCHS=1,
         LEARNING_RATE=1e-5,  # learning rate (default: 0.01)
         MAX_SRC_LEN=512,
         MAX_TGT_LEN=512,
-        DATA_DIR = "/home/song/Desktop/Auto-Code-Generator/data_processing/processed_data/finetuning",
-        RESUME_PATH = "",
+        DATA_DIR="/home/song/Desktop/AutomaticCodeGeneration/PycharmVersion/Auto-Code-Generator/data_processing/processed_data/finetuning",
+        RESUME_PATH="/home/song/Desktop/Auto-Code-Generator/best_model_loss.ckpt",
         RESUME=False
     )
 
-
     # tokenzier for encoding the text
     tokenizer = T5Tokenizer.from_pretrained("t5-base")
+    tokenizer.add_special_tokens({'additional_special_tokens': ["§", "ø"]})
+
     # T5_config = T5Config().from_pretrained("t5-base")
     # model = T5ForConditionalGeneration(config=T5_config)
     model = T5ForConditionalGeneration.from_pretrained("t5-base")
@@ -187,7 +213,7 @@ def fine_tuning():
 
     # Defining the optimizer that will be used to tune the weights of the network in the training session.
     optimizer = torch.optim.AdamW(params=model.parameters(), lr=config.LEARNING_RATE)
-    
+
     # Defining the LR scheduler with warm up
     lr_sch = LinearWarmupRsqrtDecayLR(optimizer, 1000)
 
@@ -204,31 +230,33 @@ def fine_tuning():
     metrics = {}
 
     train_interval = 100
-    val_interval = 100
+    val_interval = 500
 
     opt_level = "O1"
-    model, optimizer = amp.initialize(model,optimizer, opt_level=opt_level)
+    model, optimizer = amp.initialize(model, optimizer, opt_level=opt_level)
 
     # Load Saved Model
     if config.RESUME:
         print("Load saved model!")
         print("Path: ", config.RESUME_PATH)
-        model, optimizer, step, lr_sch = load_checkpoint(config.RESUME_PATH, model, optimizer, lr_sch, opt_level)
+        model, optimizer, step, lr_sch = load_checkpoint(config.RESUME_PATH, model, optimizer, lr_sch)
         print("step: ", step)
 
-    training_loader = data_process(config, tokenizer, "train", 5000)
-    val_loader = data_process(config, tokenizer, "val", 2000)
-    test_loader = data_process(config, tokenizer, "test", 2000)
+    # Load 2000 validation data, 23108 in total.
+    val_loader = data_process(config, tokenizer, "val", 0, 20000)
 
     print("+++++++++++++++++++++ Fine tuning ++++++++++++++++++++++++")
 
+    # training_loader = load_training_data_portion(config, tokenizer, data_portion)
+
     while step < config.TRAIN_STEPS:
 
-        # epoch == step // len(training_loader)
+        # Load portion of processed data
+        training_loader = load_training_data_portion(config, tokenizer, data_portion)
+        data_portion += 1
+        epoch = data_portion // 2
 
         for _, data in enumerate(training_loader, 0):
-
-            model.train()
 
             loss = training_per_iteration(model, tokenizer, data, optimizer, lr_sch, amp)
 
@@ -242,30 +270,30 @@ def fine_tuning():
                 train_loss_ = sum(avg_loss) / len(avg_loss)
                 train_loss.append(train_loss_)
 
-
             if step % val_interval == 0:
                 _, _, val_loss_ = validate(tokenizer, model, val_loader, generate=False)
                 val_avg_loss = sum(val_loss_) / len(val_loss_)
                 print("Average val loss ", val_avg_loss)
                 val_loss.append(val_avg_loss)
-                save_checkpoint(step, model, optimizer, lr_sch, val_avg_loss, min(val_loss), amp)  # Early stop for best Loss
+                save_checkpoint(step, model, optimizer, lr_sch, val_avg_loss, min(val_loss),
+                                amp)  # Early stop for best Loss
 
+    #  Testing
 
-     # Testing
+    test_loader = data_process(config, tokenizer, "test", 0, 20000)
+
     prediction, actual, test_loss = validate(tokenizer, model, test_loader, generate=True)
     rouge = calculate_rouge(prediction, actual)
-    metrics["rouge"].append(rouge)
     print("Rouge score", rouge)
     final_df = pd.DataFrame({'Generated Code': prediction, 'Actual Code': actual})
     final_df.to_csv('./predictions.csv')
     print('Output Files generated for review')
-    
 
     Info = {
-        "train_loss": train_loss, 
+        "train_loss": train_loss,
         "val_loss": val_loss,
-        "test_loss" : test_loss,  
-        "rouge": rouge, 
+        "test_loss": test_loss,
+        "rouge": rouge,
         "num_of_epochs": config.TRAIN_EPOCHS,
         "num_of_steps": step
     }
